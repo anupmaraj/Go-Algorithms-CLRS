5f0d908a8c1e6f2b123933b781644b892d261dad
arajideas <anupmaraj@gmail.com>
2015-11-08 19:56:48 +0100

    Insertion Sort Algorithm in ascending and descending order

---
+++ insertionsort.go
@@ -0,0 +1,40 @@
+package main 
+
+import "fmt"
+
+func insertionsortdescending(list []int){
+	for j:=1; j<len(list);j++{
+		key:=list[j]
+		i:=j-1
+		for i>=0 && list[i] < key{
+			list[i+1] = list[i]
+			i= i-1
+		}
+		list[i+1]=key
+	}
+}
+
+func insertionsortascending(list []int){
+    for j:=1; j<len(list); j++{
+        key:= list[j]
+        i:=j-1
+        for i>=0 && list[i] > key{
+            list[i+1] = list[i]
+            i = i-1
+        } 
+        list[i+1] = key
+    }
+}
+
+
+func main(){
+	list:= []int{12,4,56,22,10,7,1,0}
+	fmt.Println("\nUnsorted List=", list)
+
+	insertionsortdescending(list)
+	fmt.Println("\nDescending Sorted List=", list)	
+
+	insertionsortascending(list)
+    fmt.Println("\nAscending Sorted List", list)
+
+}
