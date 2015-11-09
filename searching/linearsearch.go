3ef17ecfcd0cabfe43aba318eda1f1d625798b31
arajideas <anupmaraj@gmail.com>
2015-11-09 15:55:47 +0100

    updated linearsearch

--- searching/linearsearch.go
+++ searching/linearsearch.go
@@ -8,22 +8,29 @@ your algorithm is correct.*/
 package main 
 
 import "fmt"
+import "math/rand"
 
 func linearsearch(a []int, v int){
 	var counter int		//to keep track of more than one occurance
 	for i:=0; i<len(a); i++{
 		if a[i]==v{
-			fmt.Println("Value at position", i+1)
+			fmt.Println("Value", v, "at position", i+1)
 			counter++
 		}
 	}
 	if counter==0{ 
-		fmt.Println("Value not found in array")
+		fmt.Println("Value", v, "not found in array")
 	}
 }
 
 func main(){
-	A := []int{4,7,2,12,6,8,4,7,9,21}
-	V := 4		//value being searched
+	n := 10							//array length
+	A := make([]int, n) 			//generate array
+	for i := 0; i <= n - 1; i++ {	//fill araay with random int values
+        A[i] = rand.Intn(n)
+    }
+    fmt.Println("Array: ",A)
+    //A:= []int{2,3,4,5,}		//in case you want to define the array yourself
+	V := 0		//value being searched
 	linearsearch(A, V)
 }
