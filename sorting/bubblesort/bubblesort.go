4fa0750fe1e2a8a0d204b8d63d826fe000a88a44
arajideas <anupmaraj@gmail.com>
2015-11-22 20:33:47 +0100

    Updating all prev changes

--- sorting/bubblesort/bubblesort.go
+++ sorting/bubblesort/bubblesort.go
@@ -1,12 +1,10 @@
 /*Bubble Sort-repeatedly steps through the list to be sorted, 
 compares each pair of adjacent items and swaps them if they 
 are in the wrong order. 
-
 Worst Case Performance: O(n^2)
 Average Case Performance:O(n^2)
 Best Case Performance:O(n^2)
 Worst Case Space Complexity:O(1) auxiliary
-
 */
 
 package main 
@@ -18,15 +16,17 @@ func BubbleSort(arr []int){
 	length := len(arr)-1			
 	for i:=0; i<length-1; i++{
 		for j:=length; j>i; j--{
-			if arr[j] < arr[j-1]{
-				arr[j], arr[j-1] = arr[j-1], arr[j]
+			//Check if previous value is greater than current value
+			if arr[j] < arr[j-1]{ 
+				//Swap previous value with current value
+				arr[j], arr[j-1] = arr[j-1], arr[j] 
 			}
 		}
 	}
 }
 
 func main(){
-	arr := []int{10,4,3,2,7,9,5,88,12,16}
+    arr := []int{10,4,3,2,7,9,5,88,12,16}
     fmt.Println("Initial array is:", arr)
     BubbleSort(arr)
     fmt.Println("Sorted array is: ", arr)
